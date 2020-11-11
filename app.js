811ad901d6eac9f4b8dbea365c490f642e107150
andika.pelealu90 <andika.pelealu90@gmail.com>
2020-11-12 02:47:17 +0700

    axios integration

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 require ('dotenv').config()
+const axios = require('axios')
 const dbot = require('dbot-js')
+const { static } = require('express')
 const Telegram = require('node-telegram-bot-api')
-
+let url = process.env.baseUrl
 const token = process.env.bot_key
+const qs = require ('qs')
 
 const talikasih_bot= new Telegram(token,{polling:true})
-// talikasih_bot.on("polling_error", (err) => console.log(err));
-
-// return
 talikasih_bot.on('message', function(message){
     let chatId = message.chat.id
-    // console.log(chatId);
-    // let message = message.text
     console.log(message.text);
-    // let result = "hai"
-    dbot.get_response(message, function(err, result){
+    // let r = axios.get (`${url}/discover/all/1`).then(response => {
+    //     let a = response.data;
+    //     console.log(a);
+    // })
+    let a = "halo"
+    dbot.get_response(message, async function(err, a){
+        let r = await axios.get (`${url}/discover/all/1`)
+        let s = r.data
+        s.forEach(el => {
+            console.log(el);
+        })
+        console.log(r.data.goal);
         if (!err) {
-            // console.log(chatId);
-            // console.log(message);
-            talikasih_bot.sendMessage(chatId, result)
+            talikasih_bot.sendMessage(chatId, a)
         } else {
             console.log(err);
         }
-    })
 
-})
+        })
+    
+    })
